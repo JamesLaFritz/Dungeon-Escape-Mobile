a367ecb4bcbae3f16c69b7ad08025810a3aa74bc
James LaFritz <ktmarine1999@gmail.com>
2021-07-01 00:41:27 -0400

    Moved Player Animator Controller Script

--- Assets/Dungeon Escape Mobile/Scripts/Player.cs
+++ Assets/Dungeon Escape Mobile/Scripts/Player.cs
@@ -1,22 +1,7 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class Player : MonoBehaviour
 {
-    private PlayerAnimationController m_animation;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_animation = GetComponent<PlayerAnimationController>();
-        if (m_animation != null && m_animation.enabled)
-        {
-            m_animation.EquipFlamingSword();
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
