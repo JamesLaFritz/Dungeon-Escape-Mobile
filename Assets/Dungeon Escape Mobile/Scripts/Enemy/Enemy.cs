1cefeae099992248507dfbbf64ca2883dddf8ae4
James LaFritz <ktmarine1999@gmail.com>
2021-07-08 10:26:10 -0400

    Enemy Base and Moss Giant Created

---
+++ Assets/Dungeon Escape Mobile/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int speed;
+    [SerializeField] protected int gems;
+
+    [SerializeField] protected float idleWaitTime;
+
+    [SerializeField] protected CinemachinePathBase waypoints;
+    protected bool hasWayPoints;
+    private int m_currentWaypoint = 1;
+    private int m_currentDirection = 1;
+    protected Vector3 currentPosition;
+    protected Vector3 nextPosition;
+    private bool m_directionChanged;
+
+    private Animator m_animator;
+    private bool m_hasAnimator;
+    private static int _idleParameterName;
+
+    protected virtual void Start()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+        if (m_animator != null)
+        {
+            m_hasAnimator = true;
+            _idleParameterName = Animator.StringToHash("Idle");
+        }
+
+        hasWayPoints = waypoints != null;
+        currentPosition = nextPosition = transform.position;
+        NextPosition();
+    }
+
+    protected virtual void Update()
+    {
+        if (m_hasAnimator && m_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            return;
+
+        if (m_directionChanged)
+        {
+            m_directionChanged = false;
+            transform.right = new Vector3(-m_currentDirection, 0, 0);
+        }
+
+        Move();
+    }
+
+    /// <summary>
+    /// Changes the current way point to the next way point
+    /// </summary>
+    protected void NextPosition()
+    {
+        if (!hasWayPoints || currentPosition != nextPosition) return;
+
+        // set the next position to the position at the current way point
+        nextPosition = waypoints.EvaluatePositionAtUnit(m_currentWaypoint, CinemachinePathBase.PositionUnits.PathUnits);
+
+        // increase the current way point
+        m_currentWaypoint += m_currentDirection;
+
+        // if the current way point is outside the bounds
+        if (!(m_currentWaypoint < waypoints.MinUnit(CinemachinePathBase.PositionUnits.PathUnits)) &&
+            !(m_currentWaypoint > waypoints.MaxUnit(CinemachinePathBase.PositionUnits.PathUnits))) return;
+
+        // change the direction
+        m_directionChanged = true;
+        m_currentDirection *= -1;
+
+        // set the current way point
+        m_currentWaypoint += m_currentDirection * 2;
+    }
+
+    /// <summary>
+    /// While the current position is not the next position, move towards the next position.
+    /// Once the enemy reaches the next position get the next position to move to and set the State to Idle
+    /// </summary>
+    protected virtual void Move()
+    {
+        // set the current position using move towards
+        currentPosition = Vector3.MoveTowards(currentPosition, nextPosition, speed * Time.deltaTime);
+        // set the transform position to the current position
+        transform.position = currentPosition;
+
+        if (currentPosition == nextPosition && m_hasAnimator)
+            m_animator.SetTrigger(_idleParameterName);
+
+        // Get the Next Position
+        NextPosition();
+    }
+
+    protected virtual IEnumerator Attack()
+    {
+        yield return null;
+    }
+
+    protected virtual IEnumerator Dead()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Dungeon Escape Mobile/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator;
+
+    #region Speed
+
+    private int m_speedParameter;
+    private bool m_hasSpeedParameter;
+
+    public void SetAnimatorMoveSpeedParameter(float value)
+    {
+        if (!m_hasSpeedParameter) return;
+        Debug.Assert(m_animator != null, nameof(m_animator) + " != null");
+        m_animator.SetFloat(m_speedParameter, value);
+    }
+
+    #endregion
+
+    #region Jump
+
+    private int m_jumpParameter;
+    private bool m_hasJumpParameter;
+
+    public void TriggerJump()
+    {
+        if (!m_hasJumpParameter) return;
+
+        Debug.Assert(m_animator != null, nameof(m_animator) + " != null");
+        m_animator.SetTrigger(m_jumpParameter);
+    }
+
+    #endregion
+
+    #region Attack
+
+    private int m_attackParameter;
+    private bool m_hasAttackParameter;
+
+    [SerializeField] private Animator m_swordAnim;
+
+    public void TriggerAttack()
+    {
+        if (!m_hasAttackParameter) return;
+
+        Debug.Assert(m_animator != null, nameof(m_animator) + " != null");
+        m_animator.SetTrigger(m_attackParameter);
+
+        if (m_swordAnim != null)
+        {
+            m_swordAnim.SetTrigger("Attack");
+        }
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+        if (m_animator == null)
+        {
+            enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Assert(m_animator != null, nameof(m_animator) + " != null");
+
+        if (m_animator.GetParameter(GetAnimatorParameterIndex("Speed")) != null)
+        {
+            m_hasSpeedParameter = true;
+            m_speedParameter = Animator.StringToHash("Speed");
+        }
+
+        if (m_animator.GetParameter(GetAnimatorParameterIndex("Jump")) != null)
+        {
+            m_hasJumpParameter = true;
+            m_jumpParameter = Animator.StringToHash("Jump");
+        }
+
+        if (m_animator.GetParameter(GetAnimatorParameterIndex("Attack")) != null)
+        {
+            m_hasAttackParameter = true;
+            m_attackParameter = Animator.StringToHash("Attack");
+        }
+    }
+
+    public byte GetAnimatorParameterIndex(string paramName)
+    {
+        for (byte i = 0; i < m_animator.parameters.Length; i++)
+        {
+            if (m_animator.parameters[i].name == paramName)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Parameter " + paramName + " doesn't exist in the animator parameter list!");
+        return 0;
+    }
+
+    public void ActivateLayer(int layerIndex)
+    {
+        m_animator.SetLayerWeight(layerIndex, 1);
+    }
+
+    public void DeactivateLayer(int layerIndex)
+    {
+        m_animator.SetLayerWeight(layerIndex, 0);
+    }
+
+    public void EquipFlamingSword()
+    {
+        ActivateLayer(m_animator.GetLayerIndex("Flame Sword"));
+    }
+
+    public void UnEquipFlamingSword()
+    {
+        DeactivateLayer(m_animator.GetLayerIndex("Flame Sword"));
+    }
+}
