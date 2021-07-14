06c6aecaac1de6922d78d49e4d90d55bd7da0f07
James LaFritz <ktmarine1999@gmail.com>
2021-07-14 12:29:09 -0400

    Damageable Code added to Moss Giant. Combat Distance set.

--- Assets/Dungeon Escape Mobile/Scripts/Enemy/MossGiant.cs
+++ Assets/Dungeon Escape Mobile/Scripts/Enemy/MossGiant.cs
@@ -3,7 +3,18 @@ public class MossGiant : Enemy, IDamageable
     #region Implementation of IDamageable
 
     /// <inheritdoc />
-    public void Damage(int amount) { }
+    public void Damage(int amount)
+    {
+        TriggerGotHit();
+
+        // Subtract amount from health
+        Health -= amount;
+
+        if (Health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     #endregion
 }
